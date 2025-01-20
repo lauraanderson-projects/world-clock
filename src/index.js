a6f6e805d06204453ebefa19e690c27328632ab2
sphirebluela <laura.sphireblue@gmail.com>
2025-01-19 22:21:33 -0800

    date/time functions added
    updated index to now utilize javascript for the time and date functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+function updateTime() {
+  //for Seattle
+  let seattleElement = document.querySelector("#seattle");
+  let seattleDateElement = seattleElement.querySelector(".date");
+  let seattleTimeElement = seattleElement.querySelector(".time");
+  seattleDateElement.innerHTML = moment()
+    .tz("America/Vancouver")
+    .format("dddd, MMMM D, YYYY");
+  seattleTimeElement.innerHTML = moment()
+    .tz("America/Vancouver")
+    .format("h:mm:ss [<small>]A[</small>]");
+  //for london
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  londonDateElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("dddd, MMMM D, YYYY");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
 
+updateTime();
+setInterval(updateTime, 1000);
